d58c2729156fcee920358938342398759a989453
ImmaDragon <devindewitt8@gmail.com>
2015-11-29 09:16:17 -0500

    hello

--- client/views/home/question3/question3.js
+++ client/views/home/question3/question3.js
@@ -16,7 +16,7 @@ Template.question3.events({
 //   questi
 // Router.go('/question2');
 
-'click #btn-one': function(){
+'click #mvp-one': function(){
 console.log('PLAY YOUTUBE SONG');
         onYouTubeIframeAPIReady = function () {
 
@@ -26,7 +26,7 @@ console.log('PLAY YOUTUBE SONG');
             height: "170",
             width: "170",
             // videoId is the "v" in URL (ex: http://www.youtube.com/watch?v=LdH1hSWGFGU, videoId = "LdH1hSWGFGU")
-            videoId: "LdH1hSWGFGU",
+            videoId: "PsO6ZnUZI0g",
             // Events like ready, state change,
             events: {
                 onReady: function (event) {
@@ -37,8 +37,78 @@ console.log('PLAY YOUTUBE SONG');
         });
     };
     YT.load();
+},
 
+'click #mvp-two': function(){
+console.log('PLAY YOUTUBE SONG');
+        onYouTubeIframeAPIReady = function () {
 
+        // New Video Player, the first argument is the id of the div.
+        // Make sure it's a global variable.
+        player = new YT.Player("player", {
+            height: "170",
+            width: "170",
+            // videoId is the "v" in URL (ex: http://www.youtube.com/watch?v=LdH1hSWGFGU, videoId = "LdH1hSWGFGU")
+            videoId: "ZyhrYis509A",
+            // Events like ready, state change,
+            events: {
+                onReady: function (event) {
+                    // Play video when player ready.
+                    event.target.playVideo();
+                }
+            }
+        });
+    };
+    YT.load();
+},
+
+'click #mvp-three': function(){
+console.log('PLAY YOUTUBE SONG');
+        onYouTubeIframeAPIReady = function () {
+
+        // New Video Player, the first argument is the id of the div.
+        // Make sure it's a global variable.
+        player = new YT.Player("player", {
+            height: "170",
+            width: "170",
+            // videoId is the "v" in URL (ex: http://www.youtube.com/watch?v=LdH1hSWGFGU, videoId = "LdH1hSWGFGU")
+            videoId: "btPJPFnesV4",
+            // Events like ready, state change,
+            events: {
+                onReady: function (event) {
+                    // Play video when player ready.
+                    event.target.playVideo();
+                }
+            }
+        });
+    };
+    YT.load();
+},
+
+'click #mvp-four': function(){
+console.log('PLAY YOUTUBE SONG');
+        onYouTubeIframeAPIReady = function () {
+
+        // New Video Player, the first argument is the id of the div.
+        // Make sure it's a global variable.
+        player = new YT.Player("player", {
+            height: "170",
+            width: "170",
+            // videoId is the "v" in URL (ex: http://www.youtube.com/watch?v=LdH1hSWGFGU, videoId = "LdH1hSWGFGU")
+            videoId: "qV5lzRHrGeg",
+            // Events like ready, state change,
+            events: {
+                onReady: function (event) {
+                    // Play video when player ready.
+                    event.target.playVideo();
+                }
+            }
+        });
+    };
+    YT.load();
 }
+
+
+
 
 }); //end of events
